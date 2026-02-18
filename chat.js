4b560acf8e8a8d188c35977cc68c40609523920e
Rahil Dave <153646627+rahildave5@users.noreply.github.com>
2026-02-19 01:40:16 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,25 @@
+const { maxHeaderSize } = require('http');
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+    from : {
+        type: String,
+        required: true,
+    },
+    to : {
+        type: String,
+        required: true,
+    },
+    message : {
+        type: String,
+        maxLength: 1000,
+    },
+    createdAt : {
+        type: Date,
+        required: true,
+    },
+});
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+module.exports = Chat;
